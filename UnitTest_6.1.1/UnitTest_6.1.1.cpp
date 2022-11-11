af23e4dff0f6024d6a57b92c1b9b60da40e5dec2
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-11 03:17:50 +0200

    commit1_master

---
+++ UnitTest_6.1.1/UnitTest_6.1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.1.1/lab_6.1.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest611
+{
+	TEST_CLASS(UnitTest611)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int r[10] = { 42,28,41,5,23,34,45,53,18,15 };
+			int t = Sum(r, 10);
+			Assert::AreEqual(t, 304);
+		}
+	};
+}
---
+++ lab_6.1.1/lab_6.1.1.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+void Show(int* r, const int a)
+{
+	for (int i = 0; i < a; i++)
+	{
+		r[i] = 4 + rand() % (68 - 4) + 1;
+	}
+}
+
+void Print(int* r, const int a)
+{
+	for (int i = 0; i < a; i++)
+	{
+		cout << r[i] << ",";
+	}
+}
+
+int Number(int* r, const int a)
+{
+	int k = 0;
+	for (int i = 0; i < a; i++)
+	{
+		if (r[i] % 2 != 0 || r[i] % 13 != 0)
+		{
+			k++;
+		}
+	}
+	return k;
+}
+
+int Sum(int* r, const int a)
+{
+	int sum = 0;
+	for (int i = 0; i < a; i++)
+	{
+		if (r[i] % 2 != 0 || r[i] % 13 != 0)
+		{
+			sum += r[i];
+
+		}
+	}
+	return sum;
+
+}
+
+void Zamina(int* r, const int a)
+{
+	for (int i = 0; i < a; i++)
+	{
+		if (r[i] % 2 != 0 || r[i] % 13 != 0)
+		{
+			r[i] = 0;
+
+		}
+	}
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int a = 23;
+	int r[a];
+	Show(r, a);
+	cout << "r {"; Print(r, a); cout << "}" << endl;
+	cout << "Number = " << Number(r, a) << endl;
+	cout << "Sum = " << Sum(r, a) << endl;
+	Zamina(r, a);
+	cout << "r {"; Print(r, a); cout << "}" << endl;
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
